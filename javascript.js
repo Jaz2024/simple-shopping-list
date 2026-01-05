0cb45aaf9a2c8d016886eb4ad9c6bfdc1d00a539
JAZVIN <164980435+Jaz2024@users.noreply.github.com>
2026-01-05 15:24:07 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,33 @@
+const ul = document.querySelector("ul");
+
+const input = document.querySelector("#item");
+
+    
+const btn = document.querySelector("button");
+btn.addEventListener("click", function (e){
+    e.preventDefault();
+
+
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+
+    span.textContent = input.value;
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", () => li.remove());
+
+    deleteBtn.style.fontWeight = "bold";
+
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+  
+
+    ul.appendChild(li);
+
+    input.value = "";
+
+    li.focus();
+    
+})
